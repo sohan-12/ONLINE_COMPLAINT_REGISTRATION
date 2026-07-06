7a7ee57848d13279dd1b0fb6d6957c0d7a48bd29
sohan-12 <sohankumarsahu402@gmail.com>
2026-07-06 22:29:58 +0530

    chore: minor server log update to trigger nodemon restart

--- server/server.js
+++ server/server.js
@@ -66,5 +66,5 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT} (online)`);
 });
